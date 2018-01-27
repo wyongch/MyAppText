f459e766ba56e31443adfcfe0a93e9f84d1edc42
wyongch <w_yongch@163.com>
2018-01-27 16:26:51 +0800

    create app test

---
+++ app/src/main/aidl/com/example/user/myapplication/Book.aidl
@@ -0,0 +1,6 @@
+// Book.aidl
+package com.example.user.myapplication;
+
+// Declare any non-default types here with import statements
+
+parcelable Book ;
---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.user.myapplication;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private BookManager mBookManager ;
+    //标志当前与服务端连接状况的布尔值，false为未连接，true为连接中
+    private boolean mBound = false;
+
+    //包含Book对象的list
+    private List<Book> mBooks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBook(v);
+
+            }
+        });
+
+    }
+
+    /**
+     * 按钮的点击事件，点击之后调用服务端的addBookIn方法
+     *
+     * @param view
+     */
+    public void addBook(View view) {
+        //如果与服务端的连接处于未连接状态，则尝试连接
+        if (!mBound) {
+            attemptToBindService();
+            Toast.makeText(this, "当前与服务端处于未连接状态，正在尝试重连，请稍后再试", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mBookManager == null) return;
+
+        Book book = new Book();
+        book.setName("APP研发录In");
+        book.setPrice(30);
+        try {
+            mBookManager.addBook(book);
+            Log.e(getLocalClassName(), book.toString());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 尝试与服务端建立连接
+     */
+    private void attemptToBindService() {
+        Intent intent = new Intent();
+        intent.setAction("com.wuyongchao.aidl");
+        intent.setPackage("com.example.user.myapplication");
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBound) {
+            attemptToBindService();
+//            this.getSystemService();
+//            getWindowManager;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.e(getLocalClassName(), "service connected");
+            mBookManager = BookManager.Stub.asInterface(service);
+            mBound = true;
+
+            if (mBookManager != null) {
+                try {
+                    mBooks = mBookManager.getBooks();
+                    Log.e(getLocalClassName(), mBooks.toString());
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.e(getLocalClassName(), "service disconnected");
+            mBound = false;
+        }
+    };
+}
